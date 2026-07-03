2ab18e1963f96ba117cde5d5d5b53869a6d19e45
0liver.kot <oliverkot19@gmail.com>
2026-07-03 21:06:43 +0100

    init

---
+++ app/src/trpc/router-type.ts
@@ -0,0 +1,4 @@
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "~/server/api/root";
+
+export type { AppRouter, inferRouterInputs, inferRouterOutputs };
